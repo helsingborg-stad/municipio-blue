75e004037d9a6b83d26f78a7e827517897e0b159
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2020-12-15 15:38:07 +0100

    Initialize 3.0/develop

---
+++ library/Theme/Enqueue.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace (#theme_namespace#)\Theme;
+
+class Enqueue
+{
+    public function __construct()
+    {
+        // Enqueue scripts and styles
+        add_action('wp_enqueue_scripts', array($this, 'style'));
+        //add_action('wp_enqueue_scripts', array($this, 'script'));
+    }
+
+    /**
+     * Enqueue styles
+     * @return void
+     */
+    public function style()
+    {
+
+        /* Removes theme parent theme css */ 
+        wp_dequeue_style('styleguide-css'); 
+        wp_dequeue_style('municipio-css'); 
+
+        wp_enqueue_style(
+            '(#theme_namespace#)',
+            get_stylesheet_directory_uri() .
+                        '/assets/dist/' .
+                        \Municipio\Helper\CacheBust::name('css/app.css', true),
+            array(),
+            ''
+        );
+    }
+
+    /**
+     * Enqueue scripts
+     * @return void
+     */
+    public function script()
+    {
+        wp_register_script(
+            '(#theme_namespace#)-js',
+            get_stylesheet_directory_uri() .
+                        '/assets/dist/' .
+                        \Municipio\Helper\CacheBust::name('js/app.js'),
+            array(),
+            false,
+            true
+        );
+
+        wp_enqueue_script('(#theme_namespace#)-js');
+    }
+}
