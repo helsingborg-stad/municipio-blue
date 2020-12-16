8808430de2b0bb5bb579011baab8e425778855ee
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-12-16 10:34:05 +0100

    Fix class loader & remove hidden files.

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ if (file_exists(dirname(ABSPATH) . '/vendor/autoload.php')) {
 }
 
 require_once MUNICIPIO_BLUE_PATH . 'library/Vendor/Psr4ClassLoader.php';
-$loader = new MUNICIPIO_BLUE\Vendor\Psr4ClassLoader();
+$loader = new MunicipioBlue\Vendor\Psr4ClassLoader();
 $loader->addPrefix('MunicipioBlue', MUNICIPIO_BLUE_PATH . 'library');
 $loader->addPrefix('MunicipioBlue', MUNICIPIO_BLUE_PATH . 'source/php/');
 $loader->register();
